7e71f4dddc36a8784c3a97b4cdbe934accd2ae6f
Angel <soporte@libus.com>
2022-11-24 15:43:03 -0300

    Primera vista por defecto

---
+++ src/components/Animation.js
@@ -0,0 +1,16 @@
+import "../styles/animation.css"
+
+export default function Animation() {
+    return (
+        <div className="view">
+            <div className="box">
+                <div className="one"><img src="/images/web30.jpg" alt="Cara 1" /></div>
+                <div className="two"><img src="/images/web20.jpg" alt="Cara 2" /></div>
+                <div className="three"><img src="/images/web10.jpg" alt="Cara 3" /></div>
+                <div className="four"><img src="/images/seguridad.jpg" alt="Cara 4" /></div>
+                <div className="five"><img src="/images/comision.jpg" alt="Cara 5" /></div>
+                <div className="six"><img src="/images/privacidad.jpg" alt="Cara 6" /></div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import "../styles/home.css"
+import Animation from "./Animation"
+
+export default function Home() {
+    return (
+        <div className="first">
+            <div className="panel-left">
+                <div className="content-top">
+                    <h2>KeyCoDev</h2>
+                    <p>
+                    Novedades de Seguridad
+                    </p>
+                </div>
+                <div className="content">
+                    <p>
+                            Tecnologias del futuro:
+                            la web 3.0 promete cambiar la
+                        forma de hacer negocios para siempre.
+                    </p>
+                    <p>
+                        Ofrecemos una alternativa diferente, cero comisión , un
+                        boton de pago seguro y de facil adaptacion
+                        a cada pagina web.
+                    </p>
+                </div>
+            </div>
+            <div className="right-panel">
+                <Animation/>
+            </div>
+        </div>
+    )
+}
